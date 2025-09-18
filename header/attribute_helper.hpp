5a25cc2d038d0970ccb9531f224caf7f4890d730
Sriram Katta <sriram.katta@fau.de>
2025-09-18 12:29:31 +0200

    Refactor attribute functions for consistency and clarity in attribute handling

--- header/attribute_helper.hpp
+++ header/attribute_helper.hpp
@@ -3,30 +3,30 @@
 #include "hdf5_utils.hpp"
 
 template <typename VT>
-void read_scalar_attribute(const H5::H5Object &obj, const std::string &attr_name, VT& value)
+void read_attribute(const H5::H5Object &obj, const std::string &attr_name, VT &value)
 {
   H5::Attribute attr = obj.openAttribute(attr_name);
   attr.read(attr.getDataType(), &value);
 }
 
-template <typename VT>
-void read_array_attribute(const H5::H5Object &obj, const std::string &attr_name, std::array<VT, 6> &values)
+template <typename VT, std::size_t N>
+void read_attribute(const H5::H5Object &obj, const std::string &attr_name, std::array<VT, N> &values)
 {
   H5::Attribute attr = obj.openAttribute(attr_name);
   attr.read(attr.getDataType(), values.data());
 }
 
-void read_string_attribute(const H5::H5Object &obj, const std::string &attr_name, std::string &value)
+template <>
+void read_attribute<std::string>(const H5::H5Object &obj, const std::string &attr_name, std::string &value)
 {
   H5::Attribute attr = obj.openAttribute(attr_name);
   H5::StrType str_type = attr.getStrType();
   attr.read(str_type, value);
 }
 
-
 // Write a scalar attribute
 template <typename VT>
-void write_scalar_attribute(const H5::H5Object &obj, const std::string &attr_name, const VT &value)
+void write_attribute(const H5::H5Object &obj, const std::string &attr_name, const VT &value)
 {
   H5::DataSpace scalar_space(H5S_SCALAR);
   H5::Attribute attr;
@@ -42,10 +42,10 @@ void write_scalar_attribute(const H5::H5Object &obj, const std::string &attr_nam
   attr.write(dtype, &value);
 }
 
-template <typename VT>
-void write_array_attribute(const H5::H5Object &obj, const std::string &attr_name, const std::array<VT, 6> &values)
+template <typename VT, std::size_t N>
+void write_attribute(const H5::H5Object &obj, const std::string &attr_name, const std::array<VT, N> &values)
 {
-  hsize_t dims[1] = {6};
+  hsize_t dims[1] = {N};
   H5::DataSpace dataspace(1, dims);
   H5::Attribute attr;
   auto dtype = get_pred_type<VT>();
@@ -61,7 +61,8 @@ void write_array_attribute(const H5::H5Object &obj, const std::string &attr_name
 }
 
 // Write a string attribute
-void write_string_attribute(const H5::H5Object &obj, const std::string &attr_name, const std::string &value)
+template <>
+void write_attribute<std::string>(const H5::H5Object &obj, const std::string &attr_name, const std::string &value)
 {
   H5::StrType str_type(H5::PredType::C_S1, value.size());
   H5::DataSpace scalar_space(H5S_SCALAR);
@@ -76,3 +77,34 @@ void write_string_attribute(const H5::H5Object &obj, const std::string &attr_nam
   }
   attr.write(str_type, value);
 }
+
+template <typename Derived>
+struct hdf5_fields_base
+{
+  void read_from_file(const H5::H5File &file, const std::string &group_name = "")
+  {
+    auto group = file.openGroup(group_name);
+    derived().for_each_attr([&](auto &&name, auto &value) {
+      read_attribute(group, name, value);
+    });
+  }
+
+  void write_to_file(const H5::H5File &file, const std::string &group_name = "") const
+  {
+    auto group = file.createGroup(group_name);
+    derived().for_each_attr([&](auto &&name, const auto &value) {
+      write_attribute(group, name, value);
+    });
+  }
+
+  void print() const
+  {
+    derived().for_each_attr([&](auto &&name, const auto &value) {
+      PRINT_VAR(value); // your macro already prints name+value
+    });
+  }
+
+private:
+  Derived &derived() { return static_cast<Derived &>(*this); }
+  const Derived &derived() const { return static_cast<const Derived &>(*this); }
+};
