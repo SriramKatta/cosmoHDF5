fd7780c53b4a727f1788faf7a6b3ea8cecfdb95b
Sriram Katta <sriram.katta@fau.de>
2025-10-07 18:03:50 +0200

    final code update

--- proj/benchmark/main.cpp
+++ proj/benchmark/main.cpp
@@ -15,65 +15,135 @@ int main(int argc, char **argv) try {
 
   std::filesystem::path p(argv[0]);
   const auto out_dirname = fmt::format("out_{}", p.filename().string());
-  auto out_file_dir      = create_out_files_dir(in_files_dir, state, out_dirname);
+  auto out_file_dir = create_out_files_dir(in_files_dir, state, out_dirname);
 
   header_group header;
   param_group params;
   config_group dconfig;
   part_groups parts;
 
+  BENCHMARK_VARS;
+
 // --------------------
 // Read
 // --------------------
 #ifdef READ_PARALLEL
-  auto in_file = create_parallel_file_handle(in_files_dir, state, H5F_ACC_RDONLY);
+  BENCHMARK(para_read_fopen, state,
+            auto in_file = create_parallel_file_handle(in_files_dir, state,
+                                                       H5F_ACC_RDONLY););
 #else
-  auto in_file = create_serial_file_handle(in_files_dir, state, H5F_ACC_RDONLY);
+  BENCHMARK(seri_read_fopen, state,
+            auto in_file =
+              create_serial_file_handle(in_files_dir, state, H5F_ACC_RDONLY););
 #endif
 
   // --------------------
   // Output file handle
   // --------------------
 #ifdef WRITE_PARALLEL
-  auto outfile_hand = create_parallel_file_handle(out_file_dir, state, H5F_ACC_TRUNC);
+  BENCHMARK(para_write_fopen, state,
+            auto outfile_hand =
+              create_parallel_file_handle(out_file_dir, state, H5F_ACC_TRUNC););
 #else
-  auto outfile_hand = create_serial_file_handle(out_file_dir, state, H5F_ACC_TRUNC);
+  BENCHMARK(seri_write_fopen, state,
+            auto outfile_hand =
+              create_serial_file_handle(out_file_dir, state, H5F_ACC_TRUNC););
 #endif
 
 
 #ifdef READ_PARALLEL
-  header.read_from_file_parallel(in_file);
-  params.read_from_file_parallel(in_file);
-  dconfig.read_from_file_parallel(in_file);
-  parts.read_from_file_parallel(in_file, state, header);
+  BENCHMARK(para_read_headers, state, {
+    header.read_from_file_parallel(in_file);
+    params.read_from_file_parallel(in_file);
+    dconfig.read_from_file_parallel(in_file);
+  });
+
+  BENCHMARK(para_read_parts, state,
+
+            { parts.read_from_file_parallel(in_file, state, header); });
 #else
-  header.read_from_file_1proc(in_file, state);
-  params.read_from_file_1proc(in_file, state);
-  dconfig.read_from_file_1proc(in_file, state);
-  parts.read_from_file_1proc(in_file, state, header);
-  header.distribute_data(state.island_comm);
-  params.distribute_data(state.island_comm);
-  dconfig.distribute_data(state.island_comm);
-  parts.distribute_data(state.island_comm);
+  BENCHMARK(seri_read_headers, state, {
+    header.read_from_file_1proc(in_file, state);
+    params.read_from_file_1proc(in_file, state);
+    dconfig.read_from_file_1proc(in_file, state);
+  });
+
+  BENCHMARK(distribute_header, state, {
+    header.distribute_data(state.island_comm);
+    params.distribute_data(state.island_comm);
+    dconfig.distribute_data(state.island_comm);
+  });
+
+  BENCHMARK(seri_read_parts, state,
+            parts.read_from_file_1proc(in_file, state, header););
+
+  BENCHMARK(distribute_parts, state,
+            { parts.distribute_data(state.island_comm); });
 #endif
+
 
 #ifdef WRITE_PARALLEL
-  header.write_to_file_parallel(outfile_hand);
-  dconfig.write_to_file_parallel(outfile_hand);
-  params.write_to_file_parallel(outfile_hand);
-  parts.write_to_file_parallel(outfile_hand, state);
+  BENCHMARK(para_write_headers, state, {
+    header.write_to_file_parallel(outfile_hand);
+    dconfig.write_to_file_parallel(outfile_hand);
+    params.write_to_file_parallel(outfile_hand);
+  });
+
+  BENCHMARK(para_write_parts, state,
+            { parts.write_to_file_parallel(outfile_hand, state); });
 #else
 
-  header.gather_data(state.island_comm);
-  dconfig.gather_data(state.island_comm);
-  params.gather_data(state.island_comm);
-  parts.gather_data(state.island_comm);
+  BENCHMARK(gather_header, state, {
+    header.gather_data(state.island_comm);
+    dconfig.gather_data(state.island_comm);
+    params.gather_data(state.island_comm);
+  });
 
-  header.write_to_file_1proc(outfile_hand, state);
-  dconfig.write_to_file_1proc(outfile_hand, state);
-  params.write_to_file_1proc(outfile_hand, state);
-  parts.write_to_file_1proc(outfile_hand, state);
+  BENCHMARK(gather_parts, state, { parts.gather_data(state.island_comm); });
+
+
+  BENCHMARK(seri_write_headers, state, {
+    header.write_to_file_1proc(outfile_hand, state);
+    dconfig.write_to_file_1proc(outfile_hand, state);
+    params.write_to_file_1proc(outfile_hand, state);
+  });
+
+  BENCHMARK(seri_write_parts, state,
+            { parts.write_to_file_1proc(outfile_hand, state); });
 #endif
+
+  auto size_island = state.island_comm.size();
+  int min_island_size{0};
+  state.world_comm.iallreduce(&size_island, &min_island_size, 1,
+                              mpicpp::op::min());
+
+  if (state.w_rank == 0) {
+    fmt::print(
+      "{:3d},"
+      "{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},"
+      "{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},"
+      "{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},"
+      "{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},"
+      "{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},"
+      "{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f},{:5.3f}\n",
+      min_island_size, MIN_para_read_fopen, MAX_para_read_fopen,
+      AVG_para_read_fopen, MIN_seri_read_fopen, MAX_seri_read_fopen,
+      AVG_seri_read_fopen, MIN_para_write_fopen, MAX_para_write_fopen,
+      AVG_para_write_fopen, MIN_seri_write_fopen, MAX_seri_write_fopen,
+      AVG_seri_write_fopen, MIN_para_read_headers, MAX_para_read_headers,
+      AVG_para_read_headers, MIN_seri_read_headers, MAX_seri_read_headers,
+      AVG_seri_read_headers, MIN_distribute_header, MAX_distribute_header,
+      AVG_distribute_header, MIN_para_read_parts, MAX_para_read_parts,
+      AVG_para_read_parts, MIN_seri_read_parts, MAX_seri_read_parts,
+      AVG_seri_read_parts, MIN_distribute_parts, MAX_distribute_parts,
+      AVG_distribute_parts, MIN_para_write_headers, MAX_para_write_headers,
+      AVG_para_write_headers, MIN_seri_write_headers, MAX_seri_write_headers,
+      AVG_seri_write_headers, MIN_gather_header, MAX_gather_header,
+      AVG_gather_header, MIN_para_write_parts, MAX_para_write_parts,
+      AVG_para_write_parts, MIN_seri_write_parts, MAX_seri_write_parts,
+      AVG_seri_write_parts, MIN_gather_parts, MAX_gather_parts,
+      AVG_gather_parts);
+  }
 
   return 0;
 } catch (...) {
--- proj/benchmark/main.hpp
+++ proj/benchmark/main.hpp
@@ -1,22 +1,61 @@
 #pragma once
 
-#include <argparse/argparse.hpp>
 #include <fmt/format.h>
+#include <argparse/argparse.hpp>
 #include <filesystem>
 
-inline std::filesystem::path parser(int argc, char **argv)
-{
-    argparse::ArgumentParser program("HDF5 MPI IO");
-    program.add_argument("infiles_dir")
-        .help("Directory containing input HDF5 files")
-        .required();
-    program.parse_args(argc, argv);
+inline std::filesystem::path parser(int argc, char **argv) {
+  argparse::ArgumentParser program("HDF5 MPI IO");
+  program.add_argument("infiles_dir")
+    .help("Directory containing input HDF5 files")
+    .required();
+  program.parse_args(argc, argv);
 
-    auto infiles_dir = std::filesystem::path(program.get<std::string>("infiles_dir"));
-    if (!std::filesystem::exists(infiles_dir) || !std::filesystem::is_directory(infiles_dir))
-    {
-        auto str = fmt::format("Input directory: {} does not exist or is not a directory\n", infiles_dir.string());
-        throw std::runtime_error(str);
-    }
-    return infiles_dir;
+  auto infiles_dir =
+    std::filesystem::path(program.get<std::string>("infiles_dir"));
+  if (!std::filesystem::exists(infiles_dir) ||
+      !std::filesystem::is_directory(infiles_dir)) {
+    auto str =
+      fmt::format("Input directory: {} does not exist or is not a directory\n",
+                  infiles_dir.string());
+    throw std::runtime_error(str);
+  }
+  return infiles_dir;
 }
+
+#define BENCHMARK_VARS                                                      \
+  double MIN_para_read_fopen{0.0}, MAX_para_read_fopen{0.0},                \
+    AVG_para_read_fopen{0.0}, MIN_seri_read_fopen{0.0},                     \
+    MAX_seri_read_fopen{0.0}, AVG_seri_read_fopen{0.0},                     \
+    MIN_para_write_fopen{0.0}, MAX_para_write_fopen{0.0},                   \
+    AVG_para_write_fopen{0.0}, MIN_seri_write_fopen{0.0},                   \
+    MAX_seri_write_fopen{0.0}, AVG_seri_write_fopen{0.0},                   \
+    MIN_para_read_headers{0.0}, MAX_para_read_headers{0.0},                 \
+    AVG_para_read_headers{0.0}, MIN_seri_read_headers{0.0},                 \
+    MAX_seri_read_headers{0.0}, AVG_seri_read_headers{0.0},                 \
+    MIN_distribute_header{0.0}, MAX_distribute_header{0.0},                 \
+    AVG_distribute_header{0.0}, MIN_para_read_parts{0.0},                   \
+    MAX_para_read_parts{0.0}, AVG_para_read_parts{0.0},                     \
+    MIN_seri_read_parts{0.0}, MAX_seri_read_parts{0.0},                     \
+    AVG_seri_read_parts{0.0}, MIN_distribute_parts{0.0},                    \
+    MAX_distribute_parts{0.0}, AVG_distribute_parts{0.0},                   \
+    MIN_para_write_headers{0.0}, MAX_para_write_headers{0.0},               \
+    AVG_para_write_headers{0.0}, MIN_seri_write_headers{0.0},               \
+    MAX_seri_write_headers{0.0}, AVG_seri_write_headers{0.0},               \
+    MIN_gather_header{0.0}, MAX_gather_header{0.0}, AVG_gather_header{0.0}, \
+    MIN_para_write_parts{0.0}, MAX_para_write_parts{0.0},                   \
+    AVG_para_write_parts{0.0}, MIN_seri_write_parts{0.0},                   \
+    MAX_seri_write_parts{0.0}, AVG_seri_write_parts{0.0},                   \
+    MIN_gather_parts{0.0}, MAX_gather_parts{0.0}, AVG_gather_parts{0.0};
+
+#define BENCHMARK(VAR, STATE, CODE)                         \
+  MIN_##VAR         = 0.0;                                 \
+  MAX_##VAR         = 0.0;                                 \
+  AVG_##VAR         = 0.0;                                 \
+  double start##VAR = MPI_Wtime();                         \
+  CODE;                                                    \
+  const auto VAR = MPI_Wtime() - start##VAR;               \
+  STATE.world_comm.iallreduce(&VAR, &MIN_##VAR, 1, mpicpp::op::min()); \
+  STATE.world_comm.iallreduce(&VAR, &MAX_##VAR, 1, mpicpp::op::max()); \
+  STATE.world_comm.iallreduce(&VAR, &AVG_##VAR, 1, mpicpp::op::sum()); \
+  AVG_##VAR /= static_cast<double>(STATE.world_comm.size());
